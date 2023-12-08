a8cf1bb6b49fb85a46454d9dfbc09c1405bda611
ko1steve <ko1steveg@gmail.com>
2023-12-08 15:16:05 +0800

    crawl web without downloading chromium

--- Program.cs
+++ Program.cs
@@ -14,42 +14,62 @@ namespace PuppeteerSharpExample
     {
         static int Main()
         {
-            Console.Write("Please enter the url : ");
-            string url = Console.ReadLine();
-            bool isSuccess = CovertHtmlToPdf(url).GetAwaiter().GetResult();
+            bool isSuccess = CovertHtmlToPdf(GetTargetUrl()).GetAwaiter().GetResult();
             return (isSuccess ? 0 : 1);
+        }
+
+        static string GetTargetUrl() {
+            bool inputUrl = Boolean.Parse(ConfigurationManager.AppSettings["INPUT_URL_FROM_CONSOLE"]);
+            if (inputUrl)
+            {
+                Console.Write("Please enter the url : ");
+                return Console.ReadLine();
+            }
+            return ConfigurationManager.AppSettings["TARGET_URL"];
         }
 
         static async Task<bool> CovertHtmlToPdf(string url)
         {
-            string pdfFileDir = ConfigurationManager.AppSettings["PDF_FILE_DIR"];
-            string pdfFileName = ConfigurationManager.AppSettings["PDF_FILE_NAME"];
-
             var launchOptions = new LaunchOptions
             {
-                Headless = true
+                Headless = Boolean.Parse(ConfigurationManager.AppSettings["HEADLESS"])
             };
-
             bool useChromeInOs = Boolean.Parse(ConfigurationManager.AppSettings["USE_CHROME_IN_OS"]);
+
             if (useChromeInOs)
             {
+                Console.WriteLine("Use Chrome installed on system OS.");
                 launchOptions.ExecutablePath = ConfigurationManager.AppSettings["CHROME_PATH"];
             }
-
-            await new BrowserFetcher().DownloadAsync();
-
+            else
+            {
+                Console.WriteLine("Downloading Chromium ...");
+                await new BrowserFetcher().DownloadAsync();
+            }
+            Console.WriteLine("Launching Chrome ...");
             using (var browser = await Puppeteer.LaunchAsync(launchOptions))
             using (var page = await browser.NewPageAsync())
             {
                 try
                 {
+                    Console.WriteLine("Open target url ...");
                     await page.GoToAsync(url);
+                    int waitForTimeout = Int32.Parse(ConfigurationManager.AppSettings["WAIT_FOR_TIMEOUT"]);
+                    if (waitForTimeout > 0) {
+                        await page.WaitForTimeoutAsync(waitForTimeout);
+                    }
                 }
                 catch (Exception ex) {
                     Console.WriteLine(ex);
                     return false;
                 }
-                if (!Directory.Exists(pdfFileDir)) { 
+
+                string pdfFileDir = ConfigurationManager.AppSettings["PDF_FILE_DIR"];
+                string pdfFileName = ConfigurationManager.AppSettings["PDF_FILE_NAME"];
+
+                if (!Directory.Exists(pdfFileDir))
+                {
+                    Console.WriteLine("Creating PDF folder ...");
                     Directory.CreateDirectory(pdfFileDir);
                 }
                 string pdfFullPath = pdfFileDir + "\\" + pdfFileName;
